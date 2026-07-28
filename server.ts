0443f62f721da80da193b34d35bd3a0e4d96e391
Hndrxx5 <118206675+Hndrxx5@users.noreply.github.com>
2026-07-27 19:40:12 -0500

    Update server.ts

--- server.ts
+++ server.ts
@@ -26,7 +26,7 @@ app.use(express.json({ limit: "10mb" }));
 // low-risk.
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   if (req.method === "OPTIONS") {
     res.sendStatus(204);
